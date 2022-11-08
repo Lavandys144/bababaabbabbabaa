6e3984d4f33dc8d276b8ec76021354fe5800f579
Lavandys144 <79376411+Lavandys144@users.noreply.github.com>
2022-11-08 18:47:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,3 @@ class Worker:
         print("Surname:", self.surname)
         print("Patronymic:", self.patronymic)
         
-
-    def add_money(self, sum):
-        self.money += sum
-        print("Зарплата", self.money)
-
-worker1 = Worker("Миша", 15, 0, None)
-worker1.info()
-worker1.add_money(10000)
-worker1.info())
-worker1.info()
